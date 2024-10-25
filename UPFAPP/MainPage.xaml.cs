aa49dac58d849be6574bd79e7bfd28bc0889f6a9
Karuna Kandel <karuna.kandel8722@nctorontostudents.ca>
2024-10-25 10:48:50 -0400

    1st commit

---
+++ UPFAPP/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Popups;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UPFAPP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private string _currentInput = "";
+        private string _pendingOperation = "";
+        private double _result = 0;
+
+        public string Result
+        {
+            get { return _currentInput; }
+            set { _currentInput = value; }
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            double n1 = (double)Convert.ToInt64(Num1.Text);
+            double n2 = (double)Convert.ToInt64(Num2.Text);
+            double result = n1 + n2;
+        }
+
+        private void sub_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            double n1 = (double)Convert.ToInt64(Num1.Text);
+            double n2 = (double)Convert.ToInt64(Num2.Text);
+            double result = n1 - n2;
+            
+        }
+
+        private void mul_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            double n1 = (double)Convert.ToInt64(Num1.Text);
+            double n2 = (double)Convert.ToInt64(Num2.Text);
+            double result = n1 * n2;
+           
+        }
+
+        private void div_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            double n1 = (double)Convert.ToInt64(Num1.Text);
+            double n2 = (double)Convert.ToInt64(Num2.Text);
+            double result = n1 / n2;
+          
+        }
+        private void UpdateResult()
+        {
+            double number;
+            if (double.TryParse(_currentInput, out number))
+            {
+                _currentInput = number.ToString();
+            }
+            else
+            {
+                _currentInput = "Error";
+            }
+        }
+
+    }
+}
